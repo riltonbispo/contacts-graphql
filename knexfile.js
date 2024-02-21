a6fb421267cebc96463e2619d9d75ebb48ec996a
riltonbispo <riltondev@gmail.com>
2024-02-21 11:44:15 -0300

    build: config knex js with postgresql

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  client: 'postgresql',
+  connection: {
+    database: 'contacts-api',
+    user: 'postgres',
+    password: '123456'
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: './src/db/migrations'
+  }
+};
